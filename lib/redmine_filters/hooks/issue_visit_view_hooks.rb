20c3b9495d5cb00c565050e2613cf83653ad19bf
Danil Tashkinov <danil.tashkinov@gmail.com>
2014-09-12 16:34:17 +0400

    fixed saving visit hook

--- lib/redmine_filters/hooks/issue_visit_view_hooks.rb
+++ lib/redmine_filters/hooks/issue_visit_view_hooks.rb
@@ -2,6 +2,7 @@ module RedmineFilters::Hooks
   class IssueVisitViewHooks < Redmine::Hook::ViewListener
     def view_issues_show_details_bottom(context = {})
       ::IssueVisit.save_visit(context[:issue]) if User.current.logged?
+      nil
     end
   end
 end
