688c425c378ac1479ad9aabf17e98e3eed549967
Danil Tashkinov <danil.tashkinov@gmail.com>
2014-09-23 13:16:07 +0400

    small design fix

--- init.rb
+++ init.rb
@@ -8,7 +8,7 @@ Redmine::Plugin.register :redmine_filters do
   name        'Redmine Filters'
   description 'This plugin adds new filters to search through history (issue participants, issue visits)'
   author      'nodecarter'
-  version     '0.0.1'
+  version     '0.0.2'
   url         'https://github.com/Undev/redmine_filters'
 
   requires_redmine :version_or_higher => '2.3.3'
